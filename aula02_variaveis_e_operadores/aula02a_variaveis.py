97c60f1c9f346d5d5043f442106219d2fd08b645
brunti <bru.aguiar2007@gmail.com>
2026-03-20 20:50:14 -0300

    Aula 02 variaveis e operadores

---
+++ aula02_variaveis_e_operadores/aula02a_variaveis.py
@@ -0,0 +1,26 @@
+print("Hello World")
+print("7+4")
+print("7"+"4")
+print(7+4)
+#comentario simples
+
+'''comentarios 
+de 
+multiplas 
+linhas
+'''
+
+#Variaveis
+nome = "Bruno" #string
+idade = 18 #int
+peso = 65 #float
+print(nome, idade, peso)
+print(f"Oiii, {nome}!!!!")
+
+#INPUT
+nome = input("Digite o seu nome: ")
+idade = int(input("Digite a sua idade:")) #converte a String para int
+
+nova_idade = idade + 1
+print(nome, idade)
+print(nova_idade)
---
+++ exercicios_aula.py
@@ -0,0 +1,15 @@
+nome = input("Digite seu nome:" )
+print("Boas-vindas", nome)
+
+dia = input("Digite o dia atual: ")
+mes = input("Digite o mes atual: ")
+ano = input("Digite o ano atual: ")
+
+print(dia+"/"+mes+"/"+ano)
+
+num1 = int(input("Digite o primeiro número:"))
+num2 = int(input("Digite o segundo número:"))
+
+soma = num1 + num2
+
+print("A soma desses números é igual a: "+soma)
